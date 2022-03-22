1462469d985fa51671cb87350a2a2863c9e0cfe3
Maximiliannnns <78545329+Maximiliannnns@users.noreply.github.com>
2022-03-22 14:30:03 +0300

    Update test22.py

--- test22.py
+++ test22.py
@@ -1,21 +1,52 @@
-from itertools import count
-import numpy as np
 import cv2
+import threading
+import numpy as np
+from pypylon import pylon
 
-
-cap = cv2.VideoCapture(0)
 count1 = 0
 
-while(True): 
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Video', frame)   
-    if cv2.waitKey(1) & 0xFF == ord('w'):
-        cv2.imwrite(f"photo{count1}.jpg", frame)
-        count1+=1
-    # cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+dist_coef = np.array([[ 2.04038247e+00, -3.23621188e+02,  1.07636960e-02, -1.16155724e-03, 1.14084132e+04]])
+camera_matrix = np.array([[5.30176732e+03, 0.00000000e+00, 1.06642613e+03],
+            [0.00000000e+00, 5.08421392e+03, 6.59997778e+02],
+            [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
-cap.release()
-cv2.destroyAllWindows()
+    #cb_func = my_callback.datastream_callback
+try:
+    # conecting to the first available camera
+    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+    # Grabing Continusely (video) with minimal delay
+    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+    converter = pylon.ImageFormatConverter()
+
+    # converting to opencv bgr format
+    converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+    converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+    print("To terminate, focus on the OpenCV window and press any key.")
+
+    while camera.IsGrabbing():
+
+        grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+        if grabResult.GrabSucceeded():
+            image = converter.Convert(grabResult)
+            img_arr = image.GetArray()
+
+            img_undist = cv2.undistort(img_arr, cameraMatrix=camera_matrix, distCoeffs=dist_coef)
+            img_undist = img_arr[300:1100, 300:1400]
+
+            cv2.imshow('img', img_undist)
+            
+
+            k = cv2.waitKey(1)
+            if k == 27:
+                cv2.imwrite(f"photo0{count1}.jpg", img_undist)
+                count1+=1
+        grabResult.Release()
+        
+    # Releasing the resource    
+    camera.StopGrabbing()
+
+except Exception as exception:
+    print(exception)
